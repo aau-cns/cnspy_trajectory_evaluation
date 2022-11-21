b37244f1b85b1ccce8e19474977e02073da44dad
rojung <roland.jung@aau.at>
2022-11-21 11:05:55 +0100

    [ADD] AbsoluteTrajectoryError.compute_absolute_global_p_global_q_error()

--- cnspy_trajectory_evaluation/AbsoluteTrajectoryError.py
+++ cnspy_trajectory_evaluation/AbsoluteTrajectoryError.py
@@ -76,6 +76,12 @@ class AbsoluteTrajectoryError:
                                                                                 q_est=traj_est.q_vec,
                                                                                 p_gt=traj_gt.p_vec,
                                                                                 q_gt=traj_gt.q_vec)
+        elif traj_err_type.is_global_p_global_q():
+            e_p_vec, e_q_vec, e_scale = \
+                AbsoluteTrajectoryError.compute_absolute_global_p_local_q_error(p_est=traj_est.p_vec,
+                                                                                q_est=traj_est.q_vec,
+                                                                                p_gt=traj_gt.p_vec,
+                                                                                q_gt=traj_gt.q_vec)
         elif traj_err_type.is_global_pose():
             e_p_vec, e_q_vec, e_scale = \
                 AbsoluteTrajectoryError.compute_absolute_global_pose_error(p_est=traj_est.p_vec,
@@ -130,6 +136,52 @@ class AbsoluteTrajectoryError:
             # R_wb_gt = R_wb_est * R_wb_err'
             # R_wb_err = R_wb_gt' * R_wb_est
             q_wb_err = q_wb_gt.conj() * q_wb_est
+
+            e_q_vec[i, :] = SpatialConverter.UnitQuaternion_to_HTMQ_quaternion(q_wb_err)
+
+
+        # scale drift
+        e_scale = AbsoluteTrajectoryError.compute_scale(p_gt, p_est)
+
+        return e_p_vec, e_q_vec, e_scale
+
+    @staticmethod
+    def compute_absolute_global_p_global_q_error(p_est, q_est, p_gt, q_gt):
+        """
+        computes the absolute cnspy_trajectory error between a estimated and ground-truth cnspy_trajectory
+        - The position error is global between the true body frame "B" and the estimated body frame "\hat{B}"
+        - expressed in the global frame
+        - The orientation error is local between the true body frame "B" and the estimated body frame "\hat{B}"
+        > p_B_Best_in_G_err =  p_GBest_in_G_est - p_GB_in_B_gt
+        > R_B_Best_err = R_G_Best_est^{T} * R_GB_gt
+        """
+        p_rows, p_cols = p_est.shape
+        q_rows, q_cols = q_est.shape
+        assert (p_est.shape == p_gt.shape)
+        assert (q_est.shape == q_gt.shape)
+        assert (p_rows == q_rows)
+        assert (p_cols == 3)
+        assert (q_cols == 4)
+
+        e_p_vec = (p_est - p_gt)
+
+        # orientation error
+        e_q_vec = np.zeros(np.shape(q_est))  # [x, y, z, w]
+
+        for i in range(np.shape(p_est)[0]):
+            q_wb_est = q_est[i, :]  # [x,y,z,w] quaternion vector
+            q_wb_gt = q_gt[i, :]  # [x,y,z,w] quaternion vector
+
+            q_wb_est = SpatialConverter.HTMQ_quaternion_to_Quaternion(q_wb_est).unit()
+            q_wb_gt = SpatialConverter.HTMQ_quaternion_to_Quaternion(q_wb_gt).unit()
+
+            # Error definitions for local perturbation:
+            # "Quaternion kinematics for the error-state Kalman filter" by Joan Solà, Chapter 4.4.1
+            # First:  S = R oplus theta  = R * Exp(theta); with R = reference/gt, theta = perturbation, S = estimate
+            # R_wb_est =  R_wb_gt  * R_wb_err
+            # R_wb_gt = R_wb_est * R_wb_err'
+            # R_wb_err = R_wb_gt' * R_wb_est
+            q_wb_err = q_wb_est.conj() * q_wb_gt
 
             e_q_vec[i, :] = SpatialConverter.UnitQuaternion_to_HTMQ_quaternion(q_wb_err)
 
